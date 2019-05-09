3a75d7fc810df8a805c4808c3e496f8100f59ade
Tianzz05 <tianzhongzheng@maka.im>
2019-05-09 18:42:44 +0800

    test git rebase

--- TestGitFlow/Startup.cs
+++ TestGitFlow/Startup.cs
@@ -25,6 +25,7 @@ namespace TestGitFlow
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO for test git rebase 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
